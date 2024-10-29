fddf685e25a47c8d6ce208fdf819230ae5756b7b
Prince Aman <111490830+Prince-2204@users.noreply.github.com>
2024-10-29 13:34:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ const express = require('express')
 
 const app = express()
 const port =3000
+app.engine('html', require('ejs').renderFile);
+
 
 app.get('/',(req,res)=>{
     res.send("Yes you just started")
 })
+app.get('/main',(req,res)=>{
 
+    res.render(__dirname + "/public/index.html")
+})
 app.listen(port, ()=>{
 
     console.log(`Running on the port ${port}`)
